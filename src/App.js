ce3ca6554b9171204623fbb3d79f8761c983a1d9
Asilbek2002 <school2924@gmail.com>
2023-08-05 22:12:08 +0500

    instalition reducer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  isLoading: false,
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "receiveDate":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "failedData":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("unknown action");
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "receiveDate", payload: data }))
+      .catch((err) => dispatch({ type: "failedData", payload: err }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
